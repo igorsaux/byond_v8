846530f48f0ff66c9c0b63885340034cc5694a8f
igorsaux <igorsa.ux@outlook.com>
2021-11-29 19:47:24 +0300

    Rename artifacts, add some shared constant paths

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -2,6 +2,7 @@
 extern crate auxtools;
 
 use auxtools::Value as ByondValue;
+use shared::SERVER_NAME;
 
 pub mod internal;
 pub mod message;
@@ -9,7 +10,7 @@ pub mod worker;
 
 #[hook("/proc/_start_v8")]
 fn start_v8() {
-  internal::start_v8("server.exe");
+  internal::start_v8(SERVER_NAME);
 
   Ok(ByondValue::null())
 }
@@ -36,25 +37,13 @@ fn execute_js(code: Value) {
 mod tests {
   use std::{thread, time::Duration};
 
+  use shared::SERVER_PATH;
+
   use crate::internal;
-
-  const fn get_server_path() -> &'static str {
-    // Lol linux users be like:
-    #[cfg(unix)]
-    return "../target/debug/server";
-
-    #[cfg(windows)]
-    return "server.exe";
-
-    #[cfg(not(windows))]
-    #[cfg(not(unix))]
-    compile_error!("Only unix or windows supported.");
-  }
 
   #[test]
   fn execute_js() {
-    let path = get_server_path();
-    internal::start_v8(path);
+    internal::start_v8(SERVER_PATH);
 
     let result = internal::execute_js("2 + 2");
     assert_eq!(result, "4");
@@ -69,7 +58,7 @@ mod tests {
 
   #[test]
   fn execute_infinite_loop() {
-    internal::start_v8(get_server_path());
+    internal::start_v8(SERVER_PATH);
 
     let result =
       internal::execute_js(r#"while (true) {}; 1"#);
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,1 +1,15 @@
 pub mod ipc;
+
+#[cfg(windows)]
+pub const SERVER_NAME: &str = "bv8.exe";
+#[cfg(unix)]
+pub const SERVER_NAME: &str = "bv8";
+
+#[cfg(windows)]
+pub const SERVER_PATH: &str = "bv8.exe";
+#[cfg(unix)]
+pub const SERVER_PATH: &str = "./target/debug/bv8";
+
+#[cfg(not(windows))]
+#[cfg(not(unix))]
+compile_error!("Only unix or windows supported.");
